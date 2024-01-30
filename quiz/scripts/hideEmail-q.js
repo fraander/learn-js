d8be7fdc9ad2f009187f950a54831e965a658ef4
fraander <57777918+fraander@users.noreply.github.com>
2024-01-30 13:19:44 -0500

    Update hideEmail-q.js

--- quiz/scripts/hideEmail-q.js
+++ quiz/scripts/hideEmail-q.js
@@ -1,11 +1,27 @@
 /* anonymizes the username(local part) of an email ID, that is, 
 replace the username part with * characters.
 */
-const emails = ['mary@northeastern.edu', 'ari@khoury.northeastern.edu', 'jk_neu.edu', 'jk@neu.edu', 'jsned@', 'ai_me@mugar.northeastern.edu'];
+const emails = [
+  "mary@northeastern.edu",
+  "ari@khoury.northeastern.edu",
+  "jk_neu.edu",
+  "jk@neu.edu",
+  "jsned@",
+  "ai_me@mugar.northeastern.edu",
+];
 window.onload = function hideEmail() {
-    const list = document.getElementById('emails')
-    list.innerHTML = '';
-    for (const email of emails) {
-        // complete the loop
-    }
-}
+  const list = document.getElementById("emails");
+  list.innerHTML = "";
+
+  for (const email of emails) {
+    symbolLocation = email.indexOf("@");
+    const updated = email
+      .split("")
+      .map((char, i) => {
+        return i < symbolLocation ? "*" : char;
+      })
+      .join("");
+    list.innerHTML += "<li>" + updated + "</li>";
+    console.log(updated);
+  }
+};
