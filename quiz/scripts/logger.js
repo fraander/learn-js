72de765ed780cb4ecd605833a9d7f656d8bc2f0d
fraander <57777918+fraander@users.noreply.github.com>
2024-01-30 16:00:49 -0500

    logger.js completed

--- quiz/scripts/logger.js
+++ quiz/scripts/logger.js
@@ -1,2 +1,15 @@
-// Define a JavaScript function called logMsg() that can be used to log an error message for any object that contains the property errMsg. 
-const logMsg = (errMsg) => console.log(errMsg)
+// Define a JavaScript function called logMsg() that can be used to log an error message for any object that contains the property errMsg.
+
+const logger = {
+  logMsg: function () {
+    if (this.errMsg) {
+      console.log(this.errMsg);
+    }
+  },
+};
+
+const obj1 = {
+  errMsg: "Error!",
+};
+
+logger.logMsg(obj1);
