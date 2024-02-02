fb0f7669c231556a5145d736294bf5ea740bbbcc
fraander <57777918+fraander@users.noreply.github.com>
2024-02-02 16:51:11 -0500

    array1-q.js

--- quiz/scripts/array1-q.js
+++ quiz/scripts/array1-q.js
@@ -1,32 +1,39 @@
-window.onload = function() {
-    const list = document.getElementById('Top5');
-    const inp = document.getElementById('inp');
-    const btn = document.getElementById('btn');
-    list.innerHTML = '';
+window.onload = function () {
+  const list = document.getElementById("Top5");
+  const inp = document.getElementById("inp");
+  const btn = document.getElementById("btn");
+  list.innerHTML = "";
 
-    const myHistory = [];
-    const MAX_HISTORY = 5;
+  const myHistory = [];
+  const MAX_HISTORY = 5;
 
-    btn.onclick = () => {
+  btn.onclick = () => {
     // we will only allow a term to be entered if the search input isn't empty
-      if (inp.value !== '') {
-              // empty the list so that we don't display duplicate entries
-              // the display is regenerated every time a search term is entered.
-              list.innerHTML = '';
+    if (inp.value !== "") {
+        console.log(inp.value)
+      myHistory.push(inp.value)
 
-              // loop through the sorted array, and display all the search terms in the list
-              for (const itemText of myHistoryCopy) {
-                
-              }
+      // empty the list so that we don't display duplicate entries
+      // the display is regenerated every time a search term is entered.
+      list.innerHTML = "";
 
-              // If the array length is 5 or more, remove the oldest search term
-              if (myHistory.length >= MAX_HISTORY) {
-                
-              }
+      // loop through the sorted array, and display all the search terms in the list
+      for (const itemText of myHistory.sort((a, b) => a.length - b.length)) {
+        console.log(itemText)
+        let li = document.createElement("li")
+        let text = document.createTextNode(itemText)
+        li.appendChild(text)
+        list.appendChild(li)
+      }
 
-              // empty the search input and focus it, ready for the next term to be entered
-              inp.value = '';
-              btn.focus();
-          }
+      // If the array length is 5 or more, remove the oldest search term
+      if (myHistory.length >= MAX_HISTORY) {
+        myHistory.pop()
+      }
+
+      // empty the search input and focus it, ready for the next term to be entered
+      inp.value = "";
+      btn.focus();
     }
-}
+  };
+};
