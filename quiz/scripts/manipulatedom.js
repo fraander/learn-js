2fb79c9507ed102103b3908fe0d3b8fbb1551952
fraander <57777918+fraander@users.noreply.github.com>
2024-02-02 16:25:41 -0500

    update so that replaced with edit text cell

--- quiz/scripts/manipulatedom.js
+++ quiz/scripts/manipulatedom.js
@@ -1,20 +1,27 @@
-window.onload = function() {
-  document.getElementById('addTableBtn').addEventListener('click', addTable);
-}
+window.onload = function () {
+  document.getElementById("addTableBtn").addEventListener("click", addTable);
+};
 
 function createTRNode(colNodes) {
   let trNode = document.createElement("tr");
-  colNodes.forEach(function(colNode) {
+  colNodes.forEach(function (colNode) {
     trNode.appendChild(colNode);
-  })
+  });
   return trNode;
 }
 
 function createTDNode(childNode) {
   let tdNode = document.createElement("td");
   tdNode.appendChild(childNode);
-  tdNode.classList = 'tdNode'
+  tdNode.classList = "tdNode";
   return tdNode;
+}
+
+function createBtn(action) {
+  let buttonNode = document.createElement("button");
+  buttonNode.textContent = "Edit text";
+  buttonNode.onclick = action;
+  return buttonNode;
 }
 
 function createTxtNode(txt) {
@@ -24,9 +31,17 @@ function createTxtNode(txt) {
 
 function addTable() {
   const tableNode = document.createElement("table");
-  for(let i = 0; i < 3; i++) {
-    let col1 = createTDNode(createTxtNode("Cell (" + i + ", 0)"));
-    tableNode.appendChild(createTRNode([col1]));
+  for (let i = 0; i < 3; i++) {
+    let textNode = createTxtNode("Cell (" + i + ", 0)");
+    let col1 = createTDNode(textNode);
+    let col2 = createTDNode(
+      createBtn(() => {
+        let fieldNode = document.createElement("input");
+        fieldNode.placeholder = "Enter cell (x, y) ...";
+        textNode.replaceWith(fieldNode);
+      })
+    );
+    tableNode.appendChild(createTRNode([col1, col2]));
   }
   document.getElementById("root").appendChild(tableNode);
 }
