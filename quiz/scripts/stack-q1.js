96cda150e82705ca78a6eaf9e61096faae57ca9b
fraander <57777918+fraander@users.noreply.github.com>
2024-02-08 14:50:44 -0500

    error checming

--- quiz/scripts/stack-q1.js
+++ quiz/scripts/stack-q1.js
@@ -16,6 +16,14 @@ class PStackImpl extends PStack {
     super();
   }
 
+  get persons() {
+    return this._persons
+  }
+
+  set persons(persons) {
+    this._persons = persons
+  }
+
   push(p) {
     return this._persons.push(p)
   }
@@ -26,8 +34,8 @@ class PStackImpl extends PStack {
 }
 
 let pstack = new PStackImpl();
-pstack.persons = [{name: 'Jojo', age: 21}, {name: 'Gabi', age: 29}]
-pstack.push({name: 'Dein', age: 19});
+pstack.persons = [{name: 'Joe', age: 21}, {name: 'Gaby', age: 29}]
+pstack.push({name: 'Devin', age: 19});
 console.log(pstack.pop());
 console.log(pstack.pop());
 console.log(pstack.persons);
